cb1e21dbcfcbedf0d5b596c2636f7408244b5fc0
Wes Baker <wes@wesbaker.com>
2013-06-04 16:34:55 -0400

    Created a Ruby script that takes a PHP file and spits out Sphinx documentation…mostly.

---
+++ method_signature.rb
@@ -0,0 +1,30 @@
+require 'mustache'
+
+class MethodSignature < Mustache
+  self.template_path = File.dirname(__FILE__)
+
+  def name
+
+  end
+
+  def arguments
+    
+  end
+
+  def comment
+    
+  end
+
+  def params
+    
+  end
+
+  def returns
+    
+  end
+
+  def rtype
+    
+  end
+
+end
