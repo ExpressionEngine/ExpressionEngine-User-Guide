17ef20d553e206a57cc3e4728c2d6c6b4afcf1f4
Andy McCormick <andy@packettide.com>
2021-12-22 14:53:02 -0500

    added tailwind and updated docs contributors page and script

--- scripts/update_contributors.sh
+++ scripts/update_contributors.sh
@@ -13,30 +13,67 @@
 
 set -eu
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-length="$(curl -s -u "$GHUSER:$GHTOKEN" https://api.github.com/repos/expressionengine/expressionengine-user-guide/contributors | jq length)"
+#Get all the contributors and output JSON to a file
+curl -s -u "$GHUSER:$GHTOKEN" https://api.github.com/repos/expressionengine/expressionengine-user-guide/contributors | jq . > contributors.json
 
-i=0
+#Get the length of the contributors JSON object
+length="$(cat contributors.json | jq length)"
+
+#Initialize our contributors string that will be used later
 CONTRIBUTORS=""
 echo "Starting Loop"
+
+#For each element in our JSON object we'll grab the users name and add it. This will allows us to sort alphabetically later
+i=0
 while [ $i -lt $length ]
 do
-    AUTHOR=$(curl -s -u "$GHUSER:$GHTOKEN" https://api.github.com/repos/expressionengine/expressionengine-user-guide/contributors | jq ".[$i] | .login" | tr -d '"')
-    IMAGE=$(curl -s -u "$GHUSER:$GHTOKEN" https://api.github.com/users/$AUTHOR | jq .avatar_url | tr -d '"')
+    AUTHOR=$(cat contributors.json | jq ".[$i] | .login" | tr -d '"')
     NAME=$(curl -s -u "$GHUSER:$GHTOKEN" https://api.github.com/users/$AUTHOR | jq .name | tr -d '"')
     if [ "$NAME" = "null" ]; then
-        NAME="$AUTHOR"
+        NAME="$(echo $AUTHOR | sed 's/.*/\L&/; s/[a-z]*/\u&/g')"
     fi
-    echo $NAME' - '$AUTHOR | tr -d '"'
-    CONTRIBUTORS+=$'\n<div class="flex-container" style="margin-bottom:10px"><div class="flex-child"><img src="'$IMAGE'" width=50 style="margin:0 10px 0 0"/></div><div class="flex-child">'$NAME'<br><a href="https://github.com/ExpressionEngine/ExpressionEngine-User-Guide/commits?author='$AUTHOR'" target="_BLANK">@'$AUTHOR'</a></div></div>'
+    tmp=$(mktemp)
+    jq ".[$i] |= . + {\"name\": \"$NAME\"}" contributors.json > "$tmp" && mv "$tmp" contributors.json
+   
     ((i=i+1))
 done
 
-cat > "$here/../docs/CONTRIBUTORS.md" <<- EOF
+#Now lets sort of JSON object alphabetically
+jq ".|=sort_by(.name)" contributors.json > "$tmp" && mv "$tmp" contributors.json
+
+#Now that we are all sorted, we're going to output our html from our JSON object.
+x=0
+while [ $x -lt $length ]
+do
+    AUTHOR=$(cat contributors.json | jq ".[$x] | .login" | tr -d '"')
+    if [[ ${AUTHOR} != *"dependabot"* ]]; then
+        IMAGE=$(cat contributors.json | jq ".[$x] | .avatar_url" | tr -d '"')
+        NAME=$(cat contributors.json | jq ".[$x] | .name" | tr -d '"')
+        
+        echo $NAME' - '$AUTHOR | tr -d '"'    
+        
+        CONTRIBUTORS+=$'\n<li><div class="space-y-4"><img class="mx-auto h-20 w-20 rounded-full lg:w-24 lg:h-24" src="'$IMAGE'" /><div class="space-y-2"><div class="text-xs font-medium lg:text-sm"><p class="mb-1">'$NAME'</p><p class="text-indigo-600"><a href="https://github.com/ExpressionEngine/ExpressionEngine-User-Guide/commits?author='$AUTHOR'" target="_BLANK">@'$AUTHOR'</a></p></div></div></div></li>'
+    fi
+    ((x=x+1))
+done
+
+rm contributors.json
+
+#Finally we output our markdown file
+cat > "$here/../docs/contributors.md" <<- EOF
 # Docs Contributors
 
-This is a list of all who have contributed content or source code to the ExpressionEngine Docs sorted ordered by most commits. If you're interested in contributing to the Docs or to the ExpressionEngine Core Project be sure to read through the [Contributing documentation](/contributing.md). 
+This is a list of all who have contributed content or source code to the ExpressionEngine Docs sorted alphabetically. If you're interested in contributing to the Docs or to the ExpressionEngine Core Project be sure to read through the [Contributing documentation](/contributing.md). 
 
-$CONTRIBUTORS
+<div class="max-w-7xl mx-auto py-12 px-4 text-center">
+<div class="space-y-8 sm:space-y-12">
+    <ul role="list" class="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-5">
+    $CONTRIBUTORS
+    </ul>
+</div>
+</div>
+
+
 
 Additionally, the following contributed content prior to 2011 when this repository was created:
 
